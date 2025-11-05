4eb67ee9db1daa2baeba9768eb086b1f1a5465e2
Mike Ackerman <dirtpathwanderer@gmail.com>
2025-11-05 09:41:50 -0700

    roxygenized package

--- R/get_project_codes.R
+++ R/get_project_codes.R
@@ -1,16 +1,22 @@
-#' @title Download PTAGIS Project Codes
+#' @title Retrieve PTAGIS Project Codes
 #'
-#' @description Download a character vector of project codes available in PTAGIS. Project
-#' codes are used to identify the mark-recapture-recovery (MRR) project under which the
-#' data was submitted. More information on available project codes can be found
-#' [here](https://www.ptagis.org/Resources/ValidationCodes?domainFilter=MrrProject).
+#' @description Retrieves a character vector of all mark-recapture-recovery (MRR)
+#' project codes currently available in the [PTAGIS](https://www.ptagis.org) database.
+#' Project codes identify the MRR project under which PIT-tag data were submitted.
+#'
+#' For a full list of available project codes with names, years, and status, see
+#' the [PTAGIS Validation Codes page](https://www.ptagis.org/Resources/ValidationCodes?domainFilter=MrrProject).
+#'
+#' @return A character vector of available project codes (uppercase, sorted, unique).
 #'
 #' @author Ryan Kinzer
 #'
-#' @export
-#' @return A character vector of available project codes (uppercase, sorted, unique).
 #' @examples
-#' \dontrun{ get_project_codes() }
+#' \dontrun{
+#' get_project_codes()
+#' }
+#'
+#' @export
 
 get_project_codes <- function() {
 
